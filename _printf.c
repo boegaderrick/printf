dad968aeb5691bbdd83e4de1f483bb1d21abe807
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-10-19 15:50:59 +0000

    first printf task

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* _printf - produces output according to format
+* @format: mandatory argument for variadic function
+* Return: length of output string
+*/
+
+int _printf(const char *format, ...)
+{
+	char *s;
+	int i, c, len = 0;
+	va_list p;
+
+	va_start(p, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				_putchar(format[i]);
+				len++;
+			}
+			if (format[i] == 'c')
+			{
+				c = va_arg(p, int);
+				_putchar(c);
+				len++;
+			}
+			if (format[i] == 's')
+			{
+				s = va_arg(p, char *);
+				while (*s != '\0')
+				{
+					_putchar(*s);
+					s++;
+					len++;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+	}
+	va_end(p);
+	return (len);
+}
